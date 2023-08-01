6539709897ca5266edadbfecc8ad3e61e71c3ebe
Iamneo <techadmin@iamneo.ai>
2023-08-01 07:31:33 +0000

    Backup Commit

--- reactapp/src/components/card/card.js
+++ reactapp/src/components/card/card.js
@@ -7,4 +7,16 @@ const Card=({key, question,correctAnswerMarkUpdate, attempt, options, answers})=
     {
         setSate(true);
     }
+    return(
+        <div>
+            <h4>
+                {question}
+            </h4>
+            <Button onClick={disableBtn} disabled = {state}>{options.option1}</Button>
+            <Button onClick={disableBtn} disabled = {state}>{options.option2}</Button>
+            <Button onClick={disableBtn} disabled = {state}>{options.option3}</Button>
+            <Button onClick={disableBtn} disabled = {state}>{options.option4}</Button>
+        </div>
+    )
 }
+export default Card;
